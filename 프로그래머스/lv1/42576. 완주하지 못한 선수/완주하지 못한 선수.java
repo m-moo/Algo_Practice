487e7679762308953ad937cd16837874b216fe69
mmoo <knr524@gmail.com>
2022-11-14 19:14:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 325.77 ms, Memory: 94.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		String rs = participant[participant.length-1];
+		for(int i=0;i<participant.length-1;i++){
+			if(!participant[i].equals(completion[i])) {
+				rs = participant[i];
+				break;
+			}
+		}
+		return rs;
+    }
+}
