3f117038364f7045cf83b9a6dc3cf1a2f7d935e8
mmoo <knr524@gmail.com>
2022-11-16 00:19:36 +0900

    [level 2] Title: 주식가격, Time: 26.47 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,14 +1,25 @@
+import java.util.Stack;
+
 class Solution {
     public int[] solution(int[] prices) {
         int len = prices.length;
         int[] answer = new int[len];
+        Stack<Integer> s = new Stack<>();
 
-        for(int i=0;i<len-1;i++) {
-            for(int j=i+1;j<len;j++){
-                answer[i]++;
-                if(prices[i]>prices[j]) break;
+        s.push(0);
+        for(int i=1;i<len;i++){
+            while(!s.isEmpty()&&prices[s.peek()]>prices[i]){
+                answer[s.peek()] = i-s.peek();
+                s.pop();
             }
+            s.push(i);
         }
+        
+        while(!s.isEmpty()){
+            answer[s.peek()] = len-s.peek()-1;
+            s.pop();
+        }
+
         return answer;
     }
 }
