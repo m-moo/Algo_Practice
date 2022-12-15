786b1c28a8d06016e7f6d2345a74acefd37819fd
mmoo <knr524@gmail.com>
2022-12-15 21:28:55 +0900

    [level 0] Title: 특이한 정렬, Time: 7.28 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -1,24 +1,11 @@
 import java.util.Arrays;
-import java.util.Comparator;
 
 class Solution {
     public int[] solution(int[] numlist, int n) {
-        int[] answer = {};
-        Integer nums[] = Arrays.stream(numlist).boxed().toArray(Integer[]::new);
-
-        Arrays.sort(nums, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                int diff1 = Math.abs(n-o1);
-                int diff2 = Math.abs(n-o2);
-                if(diff1==diff2) {
-                    return o2-o1;
-                }
-                return diff1-diff2;
-            }
-        });
-        
-        answer = Arrays.stream(nums).mapToInt(Integer::intValue).toArray();
-        return answer;
+        return Arrays.stream(numlist)
+            .boxed()
+            .sorted((a,b)-> Math.abs(a-n) == Math.abs(b-n)?
+                    b.compareTo(a):Integer.compare(Math.abs(a-n),Math.abs(b-n)))
+            .mapToInt(Integer::intValue).toArray();
     }
 }
