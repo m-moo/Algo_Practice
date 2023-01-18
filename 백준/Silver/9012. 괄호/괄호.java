0da308f89b20fa4216c7e1de4cb07ae477aa0163
mmoo <knr524@gmail.com>
2023-01-18 11:50:48 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,24 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
         for(int i=0;i<n;i++) {
-            String q = sc.next();
+            char arr[] = br.readLine().toCharArray();
             boolean flag = false;
 
             int end = 0;
-            for (int j = 0; j < q.length(); j++) {
-                if (q.charAt(j) == '(') {
-                    end++;
-                } else {
-                    end--;
-                }
-                if(end<0){
-                    break;
-                }
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] == '(') end++;
+                else end--;
+                
+                if(end<0) break;
             }
             if (end == 0) {
                 flag = true;
