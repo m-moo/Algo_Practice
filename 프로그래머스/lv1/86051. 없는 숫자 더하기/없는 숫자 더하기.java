bbb5ee5080de3705805889cefa7ffb136b240704
mmoo <knr524@gmail.com>
2023-02-18 23:45:50 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        boolean[] nums = new boolean[10];
+        for(int i=0;i<numbers.length;i++) {
+            nums[numbers[i]] = true;
+        }
+        
+        for(int i=0;i<nums.length;i++) {
+            if(!nums[i]) answer+= i;
+        }
+        return answer;
+    }
+}
