1b3371fd77bf84248d87a2d9bcd5599ce829769b
mmoo <knr524@gmail.com>
2023-02-19 11:59:26 +0900

    [level 1] Title: 신고 결과 받기, Time: 1.27 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,21 +1,23 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
 
+        List<String> distR = Arrays.stream(report).distinct().collect(Collectors.toList());
+
         HashMap<String, List<String>> rMap = new HashMap<>();
         HashMap<String,Integer> reported = new HashMap<>();
-        for(int i=0;i<report.length;i++) {
-            String[] r = report[i].split(" ");
+        for(int i=0;i<distR.size();i++) {
+            String[] r = distR.get(i).split(" ");
             List<String> mapList = rMap.getOrDefault(r[0], new ArrayList<>());
-            if(!mapList.contains(r[1])) {
-                mapList.add(r[1]);
-                rMap.put(r[0], mapList);
-                reported.put(r[1],reported.getOrDefault(r[1],0)+1);
-            }
+            mapList.add(r[1]);
+            rMap.put(r[0], mapList);
+            reported.put(r[1],reported.getOrDefault(r[1],0)+1);
         }
 
         for(int i=0;i<id_list.length;i++){
