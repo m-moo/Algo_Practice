0f8356ea190692219b5d7f7536554c65bb4debd7
mmoo <knr524@gmail.com>
2023-02-23 20:55:49 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -1,9 +1,7 @@
 class Solution {
     int answer = 0;
-    boolean[] visited;   
     
     public int solution(int[] number) {
-        visited = new boolean[number.length];
         dfs(number,0,0,0);
         return answer;
     }
@@ -11,14 +9,9 @@ class Solution {
     public void dfs(int[] number, int sum, int start, int depth) {
         if(depth < 3) {
             for(int i=start;i<number.length;i++) {
-                if(!visited[i]) {
-                    visited[i] = true;
-                    int tot = sum+number[i];
-                    if(depth == 2 && tot ==0) answer++;
-                    else dfs(number, tot,i+1, depth+1);
-
-                    visited[i] = false;
-                }
+                int tot = sum+number[i];
+                if(depth == 2 && tot ==0) answer++;
+                else dfs(number, tot,i+1, depth+1);
             }
         }
     }
