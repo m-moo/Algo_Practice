253ecfabb12776575714272dd093c416f9edd8a9
mmoo <knr524@gmail.com>
2023-02-27 17:23:38 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.35 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,31 +1,12 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
     public int solution(String s) {
         String[] nums = {"zero","one","two","three","four","five","six","seven","eight","nine"};
         int answer = 0;
         
-        StringBuffer num = new StringBuffer();
-        List<Integer> numList = new ArrayList<>();
-        for(int i=0;i<s.length();i++) {
-            char c = s.charAt(i);
-            if(c < 'A') numList.add(c-'0');
-            else{
-                num.append(c);
-                int idx = Arrays.asList(nums).indexOf(num.toString());
-                if(idx > -1) {
-                    numList.add(idx);
-                    num.delete(0,num.length());
-                }
-            }
+        for(int i=0;i<nums.length;i++) {
+            s = s.replaceAll(nums[i],Integer.toString(i));
         }
-
-        int len = numList.size();
-        for(int i=0;i<len;i++) {
-            answer += numList.get(i) * (Math.pow(10,len-1-i));
-        }
+        answer = Integer.parseInt(s);
         
         return answer;
     }
