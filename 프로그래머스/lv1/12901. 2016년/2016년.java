db34b26ebb8eb0536bb4f3d8626af9bc1614a7a0
mmoo <knr524@gmail.com>
2023-02-28 12:14:02 +0900

    [level 1] Title: 2016년, Time: 1.01 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        
+        LocalDate date = LocalDate.of(2016,a,b);
+        answer = date.getDayOfWeek().name().substring(0,3);
+        return answer;
+    }
+}
