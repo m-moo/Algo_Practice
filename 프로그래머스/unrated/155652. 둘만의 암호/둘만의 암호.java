304ecd43fb27f359324d83c43eae4cbfb5e5b63f
mmoo <knr524@gmail.com>
2023-03-15 20:33:43 +0900

    [unrated] Title: 둘만의 암호, Time: 1.06 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -3,32 +3,20 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String s, String skip, int index) {
-        ArrayList<Character> alphabet = new ArrayList<>();
-        char[] skipArr = skip.toCharArray();
-        Arrays.sort(skipArr);
+        StringBuilder answer = new StringBuilder();
 
-        int skipIdx = 0;
-        for(int i='a';i<='z';i++) {
-            if(i != skipArr[skipIdx]) alphabet.add((char)i);
-            else {
-                if(skipIdx != skipArr.length-1) skipIdx++;
+        for (char letter : s.toCharArray()) {
+            char temp = letter;
+            int idx = 0;
+            while (idx < index) {
+                temp = temp == 'z' ? 'a' : (char) (temp + 1);
+                if (!skip.contains(String.valueOf(temp))) {
+                    idx += 1;
+                }
             }
+            answer.append(temp);
         }
 
-        StringBuffer sb = new StringBuffer();
-        for(int i=0;i<s.length();i++) {
-            char c = s.charAt(i);
-            int mvIdx = alphabet.indexOf(c)+index;
-
-            while(mvIdx >= alphabet.size()) {
-                mvIdx-=alphabet.size();
-            }
-            c = alphabet.get(mvIdx);
-
-            sb.append(c);
-        }
-        
-        
-        return sb.toString();
+        return answer.toString();
     }
 }
