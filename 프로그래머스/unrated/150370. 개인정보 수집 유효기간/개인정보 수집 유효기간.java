c1b69683cda370fdbe2303a7125764a2cb5ff765
mmoo <knr524@gmail.com>
2023-03-18 14:58:31 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 3.35 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -6,35 +6,29 @@ import java.util.HashMap;
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
         ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<String, Integer> termMap = new HashMap<>();
+        int date = getDate(today);
 
-        int[] todayArr = Arrays.stream(today.split("\\.")).mapToInt(Integer::parseInt).toArray();
-        HashMap<String,Integer> termMap = new HashMap<>();
-        for(int i=0;i<terms.length;i++) {
-            String[] term = terms[i].split(" ");
+        for (String s : terms) {
+            String[] term = s.split(" ");
+
             termMap.put(term[0], Integer.parseInt(term[1]));
         }
+        for (int i = 0; i < privacies.length; i++) {
+            String[] privacy = privacies[i].split(" ");
 
-        for(int i=0;i<privacies.length;i++) {
-            String[] prv = privacies[i].split(" ");
-            int[] end = Arrays.stream(prv[0].split("\\.")).mapToInt(Integer::parseInt).toArray();
-            int term = termMap.get(prv[1]);
-
-            /* 만료 시점 구하기*/
-            end[1] += term;
-            while(end[1] > 12) {
-                end[1] -= 12;
-                end[0]++;
-            }
-            /* 만료 확인 */
-            if(todayArr[0] > end[0]) answer.add(i+1);
-            else if(todayArr[0] == end[0]) {
-                if(todayArr[1] > end[1]) answer.add(i+1);
-                else if(todayArr[1] == end[1]) {
-                    if(todayArr[2] >= end[2]) answer.add(i+1);
-                }
+            if (getDate(privacy[0]) + (termMap.get(privacy[1]) * 28) <= date) {
+                answer.add(i + 1);
             }
         }
+        return answer.stream().mapToInt(integer -> integer).toArray();
+    }
 
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+    private int getDate(String today) {
+        String[] date = today.split("\\.");
+        int year = Integer.parseInt(date[0]);
+        int month = Integer.parseInt(date[1]);
+        int day = Integer.parseInt(date[2]);
+        return (year * 12 * 28) + (month * 28) + day;
     }
 }
