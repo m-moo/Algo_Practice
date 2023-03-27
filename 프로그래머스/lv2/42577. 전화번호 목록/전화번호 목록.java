bc3b774ebf642e6c25715b5ceec9aa5baf1e6b89
mmoo <knr524@gmail.com>
2023-03-27 23:06:38 +0900

    [level 2] Title: 전화번호 목록, Time: 298.93 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,16 +1,14 @@
-import java.util.HashMap;
+import java.util.Arrays;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        HashMap<String, Integer> map = new HashMap<>();
-        for(int i = 0 ; i < phone_book.length ; i++)
-            map.put(phone_book[i],i);
-
-        for(int i = 0 ; i < phone_book.length ; i++)
-            for(int j = 1 ; j < phone_book[i].length() ; j++ )
-                if(map.containsKey(phone_book[i].substring(0,j)))  
-                    return false;       
-        return answer;
+        Arrays.sort(phone_book);
+        for(int i=0;i<phone_book.length-1;i++) {
+            int len = phone_book[i].length();
+            if(len < phone_book[i+1].length()) {
+                if(phone_book[i+1].substring(0,len).equals(phone_book[i])) return false;
+            }
+        }
+        return true;
     }
 }
