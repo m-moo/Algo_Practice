ccb5de3b985b9779291056ad5bfe0f6858eac126
mmoo <knr524@gmail.com>
2023-04-01 19:20:32 +0900

    [level 2] Title: 구명보트, Time: 9.40 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -1,28 +1,13 @@
-import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Deque;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
-        
         Arrays.sort(people);
-        Deque<Integer> dq = new ArrayDeque<>();
-        for(int i=0;i<people.length;i++) {
-            dq.add(people[i]);
+        int i = 0, j = people.length - 1;
+        for (; i < j; --j) {
+            if (people[i] + people[j] <= limit)
+                ++i;
         }
-
-        int first = dq.peekFirst();
-        while(dq.size() > 1) {
-            if(dq.peekLast() <= limit - first) {
-                dq.pollFirst();
-                first = dq.peekFirst();
-            }
-            dq.pollLast();
-            answer++;
-        }
-        if(!dq.isEmpty()) answer++;
-
-        return answer;
+        return people.length - i;
     }
 }
