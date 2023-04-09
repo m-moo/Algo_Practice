91d08043b646b4ab097360439aab464f16dde48e
mmoo <knr524@gmail.com>
2023-04-09 23:32:40 +0900

    [Silver IV] Title: 동전 0, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[n];
+        for(int i=0;i<coins.length;i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        int answer = 0;
+        for(int i=n-1;i>-1;i--) {
+            if(k == 0) break;
+            if(coins[i] <= k) {
+                answer += k/coins[i];
+                k %= coins[i];
+            }
+        }
+        System.out.println(answer);
+    }
+}
