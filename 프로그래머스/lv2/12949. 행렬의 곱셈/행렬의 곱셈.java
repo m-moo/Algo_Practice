abe22864684744b3e6671fa019c4d0a81f39db0e
mmoo <knr524@gmail.com>
2023-04-29 14:33:30 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.56 ms, Memory: 68.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,17 +1,13 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int len1 = arr1.length, len2 = arr2[0].length;
-        int cLen = Math.max(arr1[0].length,arr2[0].length);
         int[][] answer = new int[len1][len2];
-        
+
         for(int i=0;i<len1;i++) {
-            
             for(int j=0;j<len2;j++) {
-                int tot = 0;
                 for(int k=0;k<arr1[0].length;k++) {
-                    tot += arr1[i][k]*arr2[k][j];
+                    answer[i][j] += arr1[i][k]*arr2[k][j];
                 }
-                answer[i][j] = tot;
             }
         }
         
