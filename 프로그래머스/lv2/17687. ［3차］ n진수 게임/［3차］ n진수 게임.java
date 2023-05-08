854480a50080f621abeb8442aa2a1474dbcf2dab
mmoo <knr524@gmail.com>
2023-05-08 23:23:51 +0900

    [level 2] Title: [3차] n진수 게임, Time: 2.37 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,21 +1,18 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
         StringBuilder answer = new StringBuilder();
-        int cnt = 0;
 
+        int cnt = 0;
         StringBuilder sb =  new StringBuilder();
         while(sb.length() <= t*m+p) {
-            sb.append(Integer.toString(cnt,n).toUpperCase());
-            cnt++;
+            sb.append(Integer.toString(cnt++,n));
         }
-
-        cnt = p-1;
+        
         String seq = sb.toString();
-        while(answer.length() < t) {
-            answer.append(seq.charAt(cnt));
-            cnt+=m;
+        for(int i=0;i<t;i++) {
+            answer.append(seq.charAt(p-1+i*m));
         }
 
-        return answer.toString();
+        return answer.toString().toUpperCase();
     }
 }
