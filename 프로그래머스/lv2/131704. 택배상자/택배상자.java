947370407707bce8a3c3f7c2c77d341fbe5c0b81
mmoo <knr524@gmail.com>
2023-05-12 18:18:46 +0900

    [level 2] Title: 택배상자, Time: 136.35 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/lv2/131704. 택배상자/택배상자.java
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.java
@@ -3,22 +3,17 @@ import java.util.Stack;
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
-        
-        Stack<Integer> st = new Stack<>();
+        int cnt = 0;
 
-        int cnt = 1;
-        int idx = 0;
-        while(cnt <= order.length+1 && idx < order.length) {
-            if(cnt == order[idx]) {
-                answer++;
-                idx++;
-                cnt++;
-            }else {
-                if(!st.isEmpty() && st.peek() == order[idx]) {
-                    answer++;
-                    idx++;
-                    st.pop();
-                }else st.push(cnt++);
+        Stack<Integer> subLine = new Stack<>();
+        for(int i=1;i<= order.length;i++) {
+            subLine.push(i);
+
+            while(!subLine.isEmpty()) {
+                if(subLine.peek() == order[cnt]) {
+                    subLine.pop();
+                    answer++; cnt++;
+                }else break;
             }
         }
         return answer;
