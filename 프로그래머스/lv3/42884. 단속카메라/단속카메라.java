22b27c6ed3f9e75639475baeb6faab523432ee43
mmoo <knr524@gmail.com>
2023-06-02 09:10:44 +0900

    [level 3] Title: 단속카메라, Time: 9.56 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.java
@@ -1,27 +1,17 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 
 class Solution {
     public int solution(int[][] routes) {
-        Arrays.sort(routes, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0]==o2[0]?o2[1]-o1[1]:o2[0]-o1[0];
+        int answer = 0;
+
+        Arrays.sort(routes, (a, b) -> Integer.compare(a[1],b[1]));
+        int lastCam = Integer.MIN_VALUE;
+        for(int[] r:routes) {
+            if(lastCam < r[0]) {
+                answer++;
+                lastCam = r[1];
             }
-        });
-        ArrayList<Integer> cams = new ArrayList<>();
-        cams.add(routes[0][0]);
-        for(int i=1;i<routes.length;i++) {
-            boolean flag = false;
-            for(Integer c:cams) {
-                if(routes[i][0] <= c && routes[i][1] >= c) {
-                    flag = true;
-                    break;
-                }
-            }
-            if(!flag) cams.add(routes[i][0]);
         }
-        return cams.size();
+        return answer;
     }
 }
