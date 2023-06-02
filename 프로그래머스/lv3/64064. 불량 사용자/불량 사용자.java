f14c8fc32cabe9572f86206a7f3261aca1ad5b62
mmoo <knr524@gmail.com>
2023-06-02 13:05:49 +0900

    [level 3] Title: 불량 사용자, Time: 3.63 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.java
@@ -1,51 +1,29 @@
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 class Solution {
-    Set<List<String>> rs = new HashSet<>();
+    Set<Integer> set;
     
     public int solution(String[] user_id, String[] banned_id) {
-        boolean[] visited = new boolean[user_id.length];
+        set = new HashSet<>();
+        backTrack(user_id,banned_id,0,0);
 
-        backTrack(user_id,banned_id,visited,0);
-
-        return rs.size();
+        return set.size();
     }
     
-    public boolean isMatch(String userId, String bannedId) {
-        String regex = "^" + bannedId.replace("*",".") + "$";
-        Pattern pattern = Pattern.compile(regex);
-
-        Matcher matcher = pattern.matcher(userId);
-        return matcher.find();
-    }
-
-    public void backTrack(String[] user_id, String[] banned_id, boolean[] visited, int idx) {
+    // bit : 방문 순서 기록
+    public void backTrack(String[] user_id, String[] banned_id, int bit, int idx) {
         if(idx == banned_id.length) {
-            List<String> list = new ArrayList<>();
-            for(int i=0;i<visited.length;i++) {
-                if(visited[i]) list.add(user_id[i]);
-            }
-            rs.add(list);
+            set.add(bit);
             return;
         }
-
-        for(int i=0;i<user_id.length;i++){
-            if(visited[i]) continue;
-
-            if(isMatch(user_id[i], banned_id[idx])) {
-                visited[i] = true;
-                backTrack(user_id,banned_id,visited,idx+1);
-                visited[i] = false;
-            }
+        String reg = banned_id[idx].replace("*", "[\\w\\d]");
+        for(int i=0; i<user_id.length; ++i) {
+            if((((bit>>i) & 1) == 1) || !user_id[i].matches(reg)) continue;
+            backTrack(user_id, banned_id, (bit | 1<<i), idx+1);
         }
     }
 }
