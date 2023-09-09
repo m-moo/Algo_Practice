02cdb241bbdefad33463ad7f51e827c27e5be789
mmoo <knr524@gmail.com>
2023-09-10 08:20:01 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 13.16 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,44 +1,53 @@
+import java.util.ArrayList;
+
 class Solution {
     public int[] solution(int rows, int columns, int[][] queries) {
-        int[] answer = new int[queries.length];
+        ArrayList<Integer> answer = new ArrayList<>();
+        int[][] map = new int[rows+1][columns+1];
 
-        /* init board */
-        int[][] board = new int[rows][columns];
-        for(int i=0;i<rows;i++) {
-            for(int j=0;j<columns;j++) {
-                board[i][j] = i*columns+j+1;
-            }
-        }
-        int idx = 0;
-        for(int[] q:queries) {
-            int x1 = q[0]-1, y1 = q[1]-1;
-            int x2 = q[2]-1, y2 = q[3]-1;
+        for(int i=0;i<queries.length;i++) {
+            int sX = queries[i][0], sY = queries[i][1];
+            int eX = queries[i][2], eY = queries[i][3];
 
-            int tmp = board[x1][y1]; // for corner
-            int min = tmp;
-            /* rotate left side */
-            for(int i=x1;i<x2;i++) {
-                board[i][y1] = board[i+1][y1];
-                min = Math.min(min,board[i][y1]);
+            int currX = sX, currY = sY;
+            int prev = map[currX][currY];
+            if(prev == 0) prev = ((currX-1) * columns + currY);
+            int min = prev;
+
+            /* 우향 */
+            for(int k=0;k<eY-sY;k++) {
+                int curr = map[currX][++currY];
+                if(curr == 0) curr =  ((currX-1) * columns + currY);
+                min = Math.min(min,curr);
+                map[currX][currY] = prev;
+                prev = curr;
             }
-            /* rotate bottom side */
-            for(int i=y1;i<y2;i++) {
-                board[x2][i] = board[x2][i+1];
-                min = Math.min(min,board[x2][i]);
+            /* 하향 */
+            for(int k=0;k<eX-sX;k++) {
+                int curr = map[++currX][currY];
+                if(curr == 0) curr =  ((currX-1) * columns + currY);
+                min = Math.min(min,curr);
+                map[currX][currY] = prev;
+                prev = curr;
             }
-            /* rotate right side */
-            for(int i=x2;i>x1;i--) {
-                board[i][y2] = board[i-1][y2];
-                min = Math.min(min,board[i][y2]);
+            /* 좌향 */
+            for(int k=0;k<eY-sY;k++) {
+                int curr = map[currX][--currY];
+                if(curr == 0) curr =  ((currX-1) * columns + currY);
+                min = Math.min(min,curr);
+                map[currX][currY] = prev;
+                prev = curr;
             }
-            /* rotate top side */
-            for(int i=y2;i>y1;i--) {
-                board[x1][i] = board[x1][i-1];
-                min = Math.min(min,board[x1][i]);
+            /* 상향 */
+            for(int k=0;k<eX-sX;k++) {
+                int curr = map[--currX][currY];
+                if(curr == 0) curr =  ((currX-1) * columns + currY);
+                min = Math.min(min,curr);
+                map[currX][currY] = prev;
+                prev = curr;
             }
-            board[x1][y1+1] = tmp;
-            answer[idx++] = min;
+            answer.add(min);
         }
-        return answer;
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
