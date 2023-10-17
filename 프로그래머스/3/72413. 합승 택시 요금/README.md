be53f26be8bc1534a2b4a423398357c3b19924b5
mmoo <knr524@gmail.com>
2023-10-18 01:34:45 +0900

    [level 3] Title: 합승 택시 요금, Time: 19.43 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/README.md
+++ 프로그래머스/3/72413. 합승 택시 요금/README.md
@@ -1,10 +1,10 @@
 # [level 3] 합승 택시 요금 - 72413 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413) 
 
 ### 성능 요약
 
-메모리: 52.3 MB, 시간: 18.16 ms
+메모리: 52.4 MB, 시간: 19.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 1:27:25
+2023년 10월 3일 1:34:43
 
 ### 문제 설명
 
--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,32 +1,30 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int n, int s, int a, int b, int[][] fares) {
         int[][] cost = new int[n+1][n+1];
-        int max = Integer.MAX_VALUE;
+        int max = 100_000_000;
 
         // 배열 초기화
         for(int i=0;i<=n;i++) {
-            for(int j=0;j<=n;j++) {
-                cost[i][j] = i==j?0:max;
-            }
+            Arrays.fill(cost[i], max);
+            cost[i][i] = 0;
         }
         // 간선 정보 적용
         for(int[] fare:fares) {
             int to = fare[0], from= fare[1], fee = fare[2];
-            cost[to][from] = fee;
-            cost[from][to] = fee;
+            cost[to][from] = cost[from][to] = fee;
         }
         // 플로이드 와샬 적용
         for(int k=1;k<=n;k++) {
             for(int i=1;i<=n;i++) {
-                if(cost[i][k] == max) continue;
                 for(int j=1;j<=n;j++) {
-                    if(cost[k][j] == max) continue;
                     cost[i][j] = Math.min(cost[i][j], cost[i][k]+cost[k][j]);
                 }
             }
         }
         // 최소 비용 계산
-        int answer = Integer.MAX_VALUE;
+        int answer = cost[s][a] + cost[s][b];
         for(int i=1;i<=n;i++) {
             answer = Math.min(answer, cost[i][s]+cost[i][a]+cost[i][b]);
         }
