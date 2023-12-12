5994b8f9aadd683d266d87c2d26dde381cb661fd
mmoo <knr524@gmail.com>
2023-12-12 16:49:12 +0900

    [Silver III] Title: 숫자 야구, Time: 148 ms, Memory: 14520 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -1,52 +1,72 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static int limit;
+    public static int answer;
+    public static String[][] hints;
 
-        int n = sc.nextInt();
-        int[][] map = new int[n][3];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[][] hint = new String[n][3];
+
         for(int i=0;i<n;i++) {
-            int[] m = new int[3];
-            m[0] = sc.nextInt();
-            m[1] = sc.nextInt();
-            m[2] = sc.nextInt();
-            map[i] = m;
+            hint[i] = br.readLine().split(" ");
         }
-        int answer = 0;
-        for(int a=1;a<10;a++) {
-            for(int b=1;b<10;b++) {
-                for(int c=1;c<10;c++) {
-                    if(a == b || a == c || b == c) continue;
 
-                    int cnt = 0;
-                    for(int[] m:map) {
-                        int num = m[0], strike = m[1], ball = m[2];
-
-                        int aH = num/100;
-                        int bH = num/10 - aH*10;
-                        int cH = num - aH*100 - bH*10;
-
-                        int ballCnt = 0, strikeCnt = 0;
-                        if(a == aH) strikeCnt++;
-                        else if(a == bH) ballCnt++;
-                        else if(a == cH) ballCnt++;
-
-                        if(b == bH) strikeCnt++;
-                        else if(b == aH) ballCnt++;
-                        else if(b == cH) ballCnt++;
-
-                        if(c == cH) strikeCnt++;
-                        else if(c == bH) ballCnt++;
-                        else if(c == aH) ballCnt++;
-
-                        if(ball == ballCnt && strike == strikeCnt) cnt++;
-                    }
-                    if(cnt == n) answer++;
-                }
-            }
-        }
-        System.out.println(answer);
+        hints = hint;
+        answer = 0;
+        limit = n;
+        recur(0, 100);
+        System.out.print(answer);
     }
 
+    public static void recur(int hintIdx, int num) {
+        if(hintIdx == limit) {
+            answer++;
+            recur(0, num+1);
+            return;
+        }
+        if(num == 1000) return;
+
+        if(check(hintIdx, num)){
+            recur(hintIdx+1, num);
+        }else {
+            recur(0, num+1);
+        }
+    }
+
+    public static boolean check(int idx, int num) {
+        String[] hint = hints[idx];
+        char[] target = hint[0].toCharArray();
+        int strike = Integer.parseInt(hint[1]), ball = Integer.parseInt(hint[2]);
+
+        int a = num/100;
+        int b = num/10 - a*10;
+        int c = num - a*100 - b*10;
+        if(a == 0 || b == 0 || c == 0 || 
+            a == b || b == c || a == c) return false;
+
+        int ta = target[0]-'0';
+        int tb = target[1]-'0';
+        int tc = target[2]-'0';
+
+        int stCnt = 0, bCnt = 0;
+        if(a == ta) stCnt++;
+        else if(a == tb) bCnt++;
+        else if(a == tc) bCnt++;
+
+        if(b == tb) stCnt++;
+        else if(b == ta) bCnt++;
+        else if(b == tc) bCnt++;
+
+        if(c == tc) stCnt++;
+        else if(c == tb) bCnt++;
+        else if(c == ta) bCnt++;
+
+        if(strike == stCnt && ball == bCnt) return true;
+        return false;
+    }
 }
