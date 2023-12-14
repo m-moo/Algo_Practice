071fb6674d23a5e953fb86d0f97a90f5b5760f56
mmoo <knr524@gmail.com>
2023-12-14 16:30:20 +0900

    [Gold V] Title: 평범한 배낭, Time: 260 ms, Memory: 54680 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,34 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static Scanner sc = new Scanner(System.in);
+    public static int n;
+    public static int wLimit;
+    public static int[][] item;
+    public static int[][] dp;
 
-    public static void main(String[] args) {
-        int cnt = sc.nextInt();
-        int limit = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        wLimit = Integer.parseInt(st.nextToken());
 
-        int[] weight = new int[cnt+1];
-        int[] value = new int[cnt+1];
-        int[][] dp = new int[cnt+1][limit+1];
-
-        for(int i=1;i<=cnt;i++) {
-            weight[i] = sc.nextInt();
-            value[i] = sc.nextInt();
+        item = new int[n][2];
+        for(int i=0;i<n;i++){
+            item[i] = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
         }
+        dp = new int[n+1][wLimit+1];
+        System.out.println( recur(0,0));
+    }
 
-        for(int i=1;i<=cnt;i++) {
-            int currW = weight[i];
-            int currV = value[i];
-            for(int j=1;j<=limit;j++) {
-                if(currW <= j) { // 넣을 수 있는 경우
-                    // 이전의 무게에서 가능했던 최대 value와 현재 아이템을 넣었을 때
-                    // value를 비교하여 큰 쪽을 넣는다.
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-currW] + currV);
-                }else { // 넣을 수 없는 경우
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        System.out.println(dp[cnt][limit]);
+    static int recur(int idx, int weight){
+        if(weight > wLimit) return Integer.MIN_VALUE;
+        if(idx == n) return 0;
+        if(dp[idx][weight] != 0) return dp[idx][weight];
+
+        int w = item[idx][0];
+        int v = item[idx][1];
+        dp[idx][weight] = Math.max(recur(idx+1,weight+w)+v, recur(idx+1,weight));
+        return dp[idx][weight];
     }
 }
